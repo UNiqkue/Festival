c6bc25cfd99e72a5916a4c57ef999a4a43c54df2
UNiqkue <nik.nikita4@yandex.ru>
2018-10-26 14:44:26 +0300

    Sql

--- src/resources/festival_db.sql
+++ src/resources/festival_db.sql
@@ -1,43 +1,43 @@
 create table performer (
-    id bigint(20) not null,
+    id bigint(20) not null AUTO_INCREMENT,
     firstName varchar(255) not null,
     lastName varchar(255) not null,
-    username varchar(255) not null,
-    password varchar(255) not null,
+    age int(11),
+    username varchar(255),
+    password varchar(255),
     email varchar(255),
     activationCode varchar(255),
     phoneNumber varchar(255),
-    age int(11),
     duration varchar(255),
     genre varchar(255),
     primary key (id)
 );
 
 create table user (
-    id bigint(20) not null,
+    id bigint(20) not null AUTO_INCREMENT,
     firstName varchar(255) not null,
     lastName varchar(255) not null,
-    username varchar(255) not null,
-    password varchar(255) not null,
+    age int(11),
+    username varchar(255),
+    password varchar(255),
     email varchar(255),
     activationCode varchar(255),
     phoneNumber varchar(255),
-    age int(11),
     cardNumber int(16),
     primary key (id)
 );
 
 create table fest (
-    id bigint(20) not null,
+    id bigint(20) not null AUTO_INCREMENT,
     name varchar(255) not null,
-    description varchar(255) not null,
+    description varchar(255),
     countPlace int,
     price float,
     primary key (id)
 );
 
 create table fest_type (
-    evtype varchar(255),
+    type varchar(255),
     fest_id bigint(20) not null
 );
 
@@ -47,7 +47,7 @@ create table user_role (
 );
 
 create table listOfUsers (
-    id bigint(20) not null,
+    id bigint(20) not null AUTO_INCREMENT,
     list varchar(255),
     user_id bigint(20) not null,
     fest_id bigint(20) not null,
@@ -55,21 +55,33 @@ create table listOfUsers (
 );
 
 create table listOfPerformer (
-    id bigint(20) not null,
+    id bigint(20) not null AUTO_INCREMENT,
     list varchar(255),
     p_id bigint(20) not null,
     fest_id bigint(20) not null,
     primary key (id)
 );
 
-alter table listOfUsers add foreign key(user_id) references festival(id);
+alter table listOfUsers add foreign key(user_id) references user(id);
 
-alter table listOfUsers add foreign key(fest_id) references festival(id);
+alter table listOfUsers add foreign key(fest_id) references fest(id);
 
 alter table listOfPerformer add foreign key(p_id) references performer(id);
 
-alter table listOfPerformer add foreign key(fest_id) references festival(id);
+alter table listOfPerformer add foreign key(fest_id) references fest(id);
 
 alter table user_role add foreign key(user_id) references user(id);
 
-alter table fest_type add foreign key(fest_id) references user(id);
+alter table fest_type add foreign key(fest_id) references fest(id);
+
+
+Insert into user(firstname, lastname) values("Andrew", "Garfild");
+Insert into user(firstname, lastname) values("Иван", "Мирошко");
+Insert into user(firstname, lastname) values("Артём", "Чудо");
+Update user set age=24;
+Insert into user(firstname, lastname, age) values("Артём", "Чудо", 17);
+
+Insert into fest(name, description, countplace, price) values("ClassiFest", "Фестиваль классической музыки", 120, 17.0);
+Insert into fest_type(type, fest_id) values("MUSIC_FESTIVAL", 1);
+Insert into performer(firstname, lastname, age, genre) values("Владислав", "Бакей", 23, "rap");
+Insert into listofperformer(id, list, p_id, fest_id) values(1, NULL, 1, 1);
